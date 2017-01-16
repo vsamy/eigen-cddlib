5962d626a1bd669b7ca81eaf157a35010c1955e9
Vincent Samy <vincent.samy@lirmm.fr>
2017-01-16 19:06:24 +0100

    Add tests and licensing

--- tests/TestPolyhedron.cpp
+++ tests/TestPolyhedron.cpp
@@ -1,3 +1,20 @@
+// This file is part of eigen-cddlib.
+
+// eigen-cddlib is free software: you can redistribute it and/or
+// modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// eigen-cddlib is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with eigen-cddlib.  If not, see
+// <http://www.gnu.org/licenses/>.
+
 #define BOOST_TEST_MODULE TestPolyhedron
 #include <Eigen/Core>
 #include <boost/test/floating_point_comparison.hpp>
